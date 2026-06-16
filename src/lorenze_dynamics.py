95d1cae1a38e2ea871b00ab5a1cdd570cbc82a25
mandip23 <55573124+mandip23@users.noreply.github.com>
2026-06-16 14:14:17 +0545

    Update lorenze_dynamics.py

--- src/lorenze_dynamics.py
+++ src/lorenze_dynamics.py
@@ -3,23 +3,23 @@ def lorenz_equations(state,sigma=10,rho=28,beta=8/3):
     x,y,z=state
     return np.array([sigma*(y-x),x*(rho-z)-y,x*y-beta*z])
 
-def runge_kutta4(n_steps=10000,dt=0.001,rho=28):
+def runge_kutta4(n_steps=10000,dt1=0.001,rho=28):
     state=np.array([1,1,1])
     series=[]
     for _ in range(10000):
       k1=lorenz_equations(state,rho=rho)
-      k2=lorenz_equations(state+0.5*dt*k1,rho=rho)
-      k3=lorenz_equations(state+0.5*dt*k2,rho=rho)
-      k4=lorenz_equations(state+dt*k3,rho=rho)
+      k2=lorenz_equations(state+0.5*dt1*k1,rho=rho)
+      k3=lorenz_equations(state+0.5*dt1*k2,rho=rho)
+      k4=lorenz_equations(state+dt1*k3,rho=rho)
       state = state+dt/6*(k1+2*k2+2*k3+k4)
 
 
     
     for _ in range(n_steps):
       k1=lorenz_equations(state,rho=rho)
-      k2=lorenz_equations(state+0.5*dt*k1,rho=rho)
-      k3=lorenz_equations(state+0.5*dt*k2,rho=rho)
-      k4=lorenz_equations(state+dt*k3,rho=rho)
+      k2=lorenz_equations(state+0.5*dt1*k1,rho=rho)
+      k3=lorenz_equations(state+0.5*dt1*k2,rho=rho)
+      k4=lorenz_equations(state+dt1*k3,rho=rho)
       state = state+dt/6*(k1+2*k2+2*k3+k4)
       series.append(state.copy())
     
